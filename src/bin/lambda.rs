4a7b834812248b48b8e5408823acb82effe4fa96
Daniel Wagner-Hall <dawagner@gmail.com>
2024-10-10 20:50:35 +0100

    Update lambda_runtime

--- src/bin/lambda.rs
+++ src/bin/lambda.rs
@@ -1,13 +1,14 @@
-use lambda_runtime::{handler_fn, run, Context, Error};
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = handler_fn(func);
+    let func = service_fn(func);
     run(func).await
 }
 
-async fn func(_: serde_json::Value, _: Context) -> Result<String, std::string::FromUtf8Error> {
+async fn func(_: LambdaEvent<serde_json::Value>) -> Result<String, String> {
     let mut out = Vec::new();
     annual_events::make_calendar(&mut out).unwrap();
     String::from_utf8(out)
+        .map_err(|err| format!("Failed to parse calendar output as UTF-8: {:?}", err))
 }
